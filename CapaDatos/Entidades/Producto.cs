cb15c82c80bcb33ffeeea78afb3c54c4d5319d50
KatheLJ <katheloajar257@gmail.com>
2023-04-08 08:46:57 -0600

    Cambios

--- CapaDatos/Entidades/Producto.cs
+++ CapaDatos/Entidades/Producto.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace CapaDatos.Entidades
@@ -12,12 +13,16 @@ namespace CapaDatos.Entidades
     
     public class Producto
     {
+        [Display(Name = "ID")]
         public int IdProducto { get; set; }
 
+        [Display(Name = "Nombre")]
         public string NomProducto { get; set; }
 
+        [Display(Name = "Marca")]
         public int MarcaProducto { get; set; }
 
+        [Display(Name = "Precio")]
         public decimal CostoProducto { get; set; }
 
     }
